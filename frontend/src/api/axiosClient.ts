7475968f676d782cf6fc21338c0a2edb20e09443
Duy Tu <duytu178204@gmail.com>
2025-09-19 23:23:35 +0700

    đăng kí đã ok hơn

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,15 +1,3 @@
-// import axios from "axios";
-// import { API_BASE } from "./endpoints";
-
-// const client = axios.create({ baseURL: API_BASE });
-
-// client.interceptors.request.use((cfg) => {
-//   const t = localStorage.getItem("access_token");
-//   if (t) cfg.headers.Authorization = `Bearer ${t}`;
-//   return cfg;
-// });
-
-// export default client;
 
 
 // src/api/axiosClient.ts
